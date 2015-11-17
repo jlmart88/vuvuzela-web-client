ef2d1dee11cc69d68b4b629041c6ae6bf2a78cc0
Justin Martinez <jlmart88@mit.edu>
2015-11-16 23:39:26 -0500

    Dialing protocol using marshaling

--- app/services/dialing-protocol.js
+++ app/services/dialing-protocol.js
@@ -14,19 +14,23 @@ export default Ember.Service.extend({
     nextDialRequest: function(round, buckets) {
         var nextRequest = this.get('userDialRequests').shiftObject(),
             intro,
+            intro_attrs,
             packed,
             ctxt,
             res,
-            ex;
+            ex,
+            ex_attrs;
         console.log("Next dial request: "+nextRequest);
         if (nextRequest) {
             intro = {
                 'Rendezvous': round + 4,
                 'LongTermKey': this.get('session').getPublicKeyBytes()
             };
-            packed = new Uint8Array(36);
-            packed.set(new Uint8Array(new Uint32Array([intro.Rendezvous]).buffer));
-            packed.set(intro.LongTermKey, 4);
+            intro_attrs = [
+                {name:'Rendezvous', type:'uint32_t'},
+                {name:'LongTermKey', type:'uint8_t', len:32}
+            ];
+            packed = vuvuzela.marshal(intro, intro_attrs);
             ctxt = onionbox.seal(packed, vuvuzela.forwardNonce(round),[nextRequest]);
             console.log(ctxt.onion);
             ex = {
@@ -41,10 +45,11 @@ export default Ember.Service.extend({
                 'EncryptedIntro': intro
             };
         }
-
-        packed = new Uint8Array(4 + vuvuzela.sizeEncryptedIntro);
-        packed.set(new Uint8Array(new Uint32Array([ex.Bucket]).buffer));
-        packed.set(ex.EncryptedIntro, 4);
+        ex_attrs = [
+            {name:'Bucket', type:'uint32_t'},
+            {name:'EncryptedIntro', type:'uint8_t', len:vuvuzela.sizeEncryptedIntro}
+        ];
+        packed = vuvuzela.marshal(ex, ex_attrs);
         console.log("ex: "+packed);
         res = onionbox.seal(packed, vuvuzela.forwardNonce(round), this.get('session').get('serverKeys'));
         console.log("res.onion length: " + res.onion.length);
@@ -58,6 +63,7 @@ export default Ember.Service.extend({
         var nonce = vuvuzela.forwardNonce(dialBucket.Round),
             encIntro,
             intro,
+            intro_attrs,
             data,
             pk;
 
@@ -66,10 +72,11 @@ export default Ember.Service.extend({
             pk = encIntro.slice(0,32);
             try {
                 data = nacl.crypto_box_open(encIntro.slice(32), nonce, pk, this.get('session').getPrivateKeyBytes());
-                intro = {
-                    'Rendezvous': new Uint32Array(data.buffer, 0, 1),
-                    'LongTermKey': new Uint8Array(data.buffer, 4)
-                };
+                intro_attrs = [
+                    {name:'Rendezvous', type:'uint32_t'},
+                    {name:'LongTermKey', type:'uint8_t', len:32}
+                ];
+                intro = vuvuzela.unmarshal(data, intro_attrs);
             } catch (e) {
                 continue;
             }   
